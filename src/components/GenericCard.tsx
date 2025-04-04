22f886c2db67ffeefe91fd99cb7931b9c5f9e9dd
User Name <User Email>
2025-04-04 20:56:43 +0200

    Added ebent handler for child card

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -1,22 +1,30 @@
 import { Box, Typography, Button } from '@mui/material';
 import { Card, CardContent, CardActions, CardMedia } from '@mui/material';
 
+type removeItemFunction = (cardid:number) => void;
 
 //This interface defines an property called 'fields' with property 'content'
 export interface IGenericCardProps { 
     fields: {
         content: string;
-    }
-}
+   },
+   index: number;
+   removeItem?: removeItemFunction
+    
+};
+    
+
 
 export const GenericCard = (props: IGenericCardProps) => {
 
-        const { fields } = props;
+        //removeItem is defined in the parent and used to remove this GenericCard via the property 'index'
+        const { fields , index, removeItem } = props;
 
     return (
 
 
-    <Card sx={{m:3, boxShadow:8}}>
+    // Added the data-cardid custom data attribute in order to be able to track the component in the parent 
+    <Card sx={{m:3, boxShadow:8}} data-cardid={index}> 
             <CardMedia 
                 image="https://picsum.photos/200/200" 
                 component="img"
@@ -26,8 +34,9 @@ export const GenericCard = (props: IGenericCardProps) => {
                 <Typography variant="body2"  gutterBottom component="div" color="text.secondary">Nisi eu qui laboris ad veniam elit mollit commodo velit consectetur tempor. Ad ad sunt mollit tempor velit eiusmod veniam non culpa ex ex elit irure consectetur. Commodo excepteur ut cupidatat fugiat aute consectetur cillum sint id ex ad labore esse. Nostrud laboris aute eiusmod aliqua veniam ut in in excepteur magna nulla irure. Consectetur culpa laboris exercitation sit quis do est ut dolore exercitation cillum et.</Typography>   
             </CardContent>
             <CardActions>
-                <Button variant="text" size="small">Add Card</Button>
-                <Button variant="contained" size="small">Remove Card</Button>
+                {/* This will enure that the removeItem function is defined in order to satisfy the compiler requirmeent  */}
+                {removeItem && (
+                <Button variant="contained" size="small" onClick={() => removeItem(index)}>Remove Card</Button> )}
             </CardActions>
         </Card>
 
